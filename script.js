6fc136033755c20247625edb1648c947d7affdc0
edscamera <42257904+edscamera@users.noreply.github.com>
2024-09-17 22:57:28 -0400

    website

---
+++ script.js
@@ -0,0 +1,20 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+$$(".flip-card").forEach(card => {
+    const img = card.querySelector(".flip-card-inner img");
+    if (img) img.addEventListener("click", () => {
+        card.classList.toggle("flipped");
+    });
+});
+$$(".video").forEach(video => {
+    const thumbnail = video.querySelector("img");
+    if (thumbnail) thumbnail.addEventListener("click", () => {
+        const iframe = document.createElement("iframe");
+        const videoId = thumbnail.src.split("/").slice(-2)[0];
+        iframe.setAttribute("allowfullscreen", "");
+        iframe.setAttribute("allow", "autoplay");
+        iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        thumbnail.replaceWith(iframe)
+    });
+});
